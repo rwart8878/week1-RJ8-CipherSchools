7f802a697071d97fdc90c4ed0396d7803aeab170
Rupam Tiwary <Rupamtiwary258@gmail.com>
2023-01-25 19:37:30 +0530

    initial commit

--- javascript nd string/javascript node string.js
+++ javascript nd string/javascript node string.js
@@ -232,9 +232,61 @@
 // map methiod
 
 // const numbers = [3,4,5,6,7];
-// const square = function(numbers){
+// const square = (number)=>{
 //     return number * number;
 // }
 // numbers.map(square);
+// console.log(numbers.map(square));
+
+// .map based on array
+
+// let array =[1,2,3,4,5];
+// function functionForMap(element){
+//     return element * element;
+// }
+// let newArray = array.map(functionForMap);
+// console.log(newArray);
+// for(let i = 0; i<array.length;i++){
+//     newArray[i]=array[i]*array[i];
+// }
+// console.log(newArray);
 
 
+// let array=[5,6,7,8,9];
+// let newArray = array.map((element)=>{
+//     return element * element;
+// });
+// console.log(newArray);
+
+
+//  for each 
+
+// let array = [1,2,3,4,5,6,7];
+// array.forEach((element)=>{
+//     console.log(element);
+// })
+
+
+// /filter
+
+// let array = [10,20,30,40,50];
+// let newArray = array.filter((element) =>  element >= 30);
+// console.log(newArray);
+
+// .find
+
+
+// let array = [10,20,30,40,50];
+// let temp = array.find((value)=>{
+//     return value>20;
+// });
+// console.log(temp);
+ 
+//  .sort
+let array = [1,23,4,4,5,6];
+let sortedArray = array.sort((el1,el2)=>{
+    el1=Number(el1);
+    el2=Number(el2);
+    return el1-el2;
+});
+console.log(sortedArray);
